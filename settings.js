2a3ac9ea2b193908aaac26ad302add402c570d72
BOTZ-NABIL <92030642+botz-nabil@users.noreply.github.com>
2023-06-01 21:14:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'NABNAB 01'// yourname
+global.MONGO_DB_URI = "mongodb+srv://nabpay27:<password>@cluster0.yuymxtp.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "Nabil_123" //isi apa aja bebas
+global.your_email = "tg717131@gmail.com" //email
+global.email_password = "xqgsginptegotgao" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
